4729179b23fe0952adf2d60a08b868da2c95861e
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-02-27 12:58:37 -0700

    Write tests for crud functionality of experiences

--- test/system/experiences_test.rb
+++ test/system/experiences_test.rb
@@ -1,6 +1,9 @@
 require "application_system_test_case"
 
 class ExperiencesTest < ApplicationSystemTestCase
+  setup do
+    @experience = experiences(:first) # Reference to the first fixture experience
+  end
   test "Creating a new experience" do
     # When we visit the experiences#index page
     # we expect to see a title with the text "experiences"
@@ -21,5 +24,34 @@ class ExperiencesTest < ApplicationSystemTestCase
     # and to see our "Capybara experience" added to the list
     assert_selector "h1", text: "experiences"
     assert_text "Capybara experience"
+  end
+
+  test "Showing a experience" do
+    visit experiences_path
+    click_link @experience.name
+
+    assert_selector "h1", text: @experience.name
+  end
+
+  test "Updating a experience" do
+    visit experiences_path
+    assert_selector "h1", text: "experiences"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit experience"
+
+    fill_in "Name", with: "Updated experience"
+    click_on "Update experience"
+
+    assert_selector "h1", text: "experiences"
+    assert_text "Updated experience"
+  end
+
+  test "Destroying a experience" do
+    visit experiences_path
+    assert_text @experience.name
+
+    click_on "Delete", match: :first
+    assert_no_text @experience.name
   end
 end
