ba488336e1273dd0e881ba8291e1446d411821ef
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-02-27 12:54:15 -0700

    Write tests for expeirences page

--- test/system/experiences_test.rb
+++ test/system/experiences_test.rb
@@ -1,9 +1,25 @@
 require "application_system_test_case"
 
 class ExperiencesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit experiences_url
-  #
-  #   assert_selector "h1", text: "Experiences"
-  # end
+  test "Creating a new experience" do
+    # When we visit the experiences#index page
+    # we expect to see a title with the text "experiences"
+    visit experiences_path
+    assert_selector "h1", text: "experiences"
+
+    # When we click on the link with the text "New experience"
+    # we expect to land on a page with the title "New experience"
+    click_on "New experience"
+    assert_selector "h1", text: "New experience"
+
+    # When we fill in the name input with "Capybara experience"
+    # and we click on "Create experience"
+    fill_in "Name", with: "Capybara experience"
+    click_on "Create experience"
+
+    # We expect to be back on the page with the title "experiences"
+    # and to see our "Capybara experience" added to the list
+    assert_selector "h1", text: "experiences"
+    assert_text "Capybara experience"
+  end
 end
