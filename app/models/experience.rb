3de78089e7717d4811caf80e8e0e5fc3098c6be8
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-03-05 14:29:57 -0700

    Add broadcasting callbacks to Experience model for C.U.D.

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,4 +2,10 @@ class Experience < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "experiences" }
+  # the above is equivalent to:
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_update_commit -> { broadcast_replace_to "experiences" }
+  after_destroy_commit -> { broadcast_remove_to "experiences" }
 end
