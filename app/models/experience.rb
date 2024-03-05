f3bd408a5c9d92e3f6d5018322c6617265dab1c9
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-03-05 14:37:08 -0700

    refactor code to be asynch using ActiveJob and make code more consice

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,9 +3,12 @@ class Experience < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "experiences" }
-  # the above is equivalent to:
-  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
-  after_update_commit -> { broadcast_replace_to "experiences" }
-  after_destroy_commit -> { broadcast_remove_to "experiences" }
+  broadcasts_to -> (experience) { "experiences" }, inserts_by: :prepend
+  # Line 6 is equivalent to:
+    # after_create_commit -> { broadcast_prepend_later_to "experiences" }
+    # the above is equivalent to:
+    # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+    
+    # after_update_commit -> { broadcast_replace_later_to "experiences" }
+    # after_destroy_commit -> { broadcast_remove_to "experiences" }
 end
