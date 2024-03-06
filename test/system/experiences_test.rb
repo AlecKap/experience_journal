38251e752057a00f35f1dfb2c0db32c3d9ea65df
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-03-05 18:51:34 -0700

    Update page titles in experiences_test.rb

--- test/system/experiences_test.rb
+++ test/system/experiences_test.rb
@@ -15,29 +15,29 @@ class ExperiencesTest < ApplicationSystemTestCase
 
   test "Creating a new experience" do
     visit experiences_path
-    assert_selector "h1", text: "Experiences"
+    assert_selector "h1", text: "Experinotes"
 
     click_on "New Experience"
     fill_in "Name", with: "Capybara experience"
     
-    assert_selector "h1", text: "Experience"
+    assert_selector "h1", text: "Experinotes"
     click_on "Create experience"
 
-    assert_selector "h1", text: "Experiences"
+    assert_selector "h1", text: "Experinotes"
     assert_text "Capybara experience"
   end
 
   test "Updating an experience" do
     visit experiences_path
-    assert_selector "h1", text: "Experiences"
+    assert_selector "h1", text: "Experinotes"
     
     click_on "Edit", match: :first
     fill_in "Name", with: "Updated experience"
     
-    assert_selector "h1", text: "Experiences"
+    assert_selector "h1", text: "Experinotes"
     click_on "Update experience"
 
-    assert_selector "h1", text: "Experiences"
+    assert_selector "h1", text: "Experinotes"
     assert_text "Updated experience"
   end
 
