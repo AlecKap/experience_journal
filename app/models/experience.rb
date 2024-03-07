a62cd314e69b62268a67e63505db063ec346fd22
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-03-07 14:36:38 -0700

    Update spelling in comment

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -9,7 +9,7 @@ class Experience < ApplicationRecord
   # Line 6 is equivalent to:
     # after_create_commit -> { broadcast_prepend_later_to "experiences" }
     # the above is equivalent to:
-    # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+    # after_create_commit -> { broadcast_prepend_to "experiences", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
     
     # after_update_commit -> { broadcast_replace_later_to "experiences" }
     # after_destroy_commit -> { broadcast_remove_to "experiences" }
