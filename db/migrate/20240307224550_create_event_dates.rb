a0b77f7c2b1873cb4c51fe2fba2d825b7b4a7e4f
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-03-07 15:51:04 -0700

    Add migration for creating event_dates table

---
+++ db/migrate/20240307224550_create_event_dates.rb
@@ -0,0 +1,13 @@
+class CreateEventDates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :event_dates do |t|
+      t.references :experience, null: false, foreign_key: true
+      t.date :date, null: false
+
+      t.timestamps
+    end
+
+    add_index :event_dates, [:date, :experience_id], unique: true
+    add_index :event_dates, :date
+  end
+end
