9c05fd339dd71cf01e1340a1d43745233dab00ee
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-03-20 16:40:25 -0600

    Refactor devise modules in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   has_many :experiences, dependent: :destroy
-  
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :validatable, :rememberable, :registerable
-        #  :recoverable
+  # :lockable, :timeoutable, :trackable and :omniauthable, :recoverable
+  devise :database_authenticatable,
+         :validatable,
+         :rememberable,
+         :registerable,
+         :confirmable
+        
 
 
   def name
